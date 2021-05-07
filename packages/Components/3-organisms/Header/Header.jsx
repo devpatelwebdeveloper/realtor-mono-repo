51de22b2749644e60b52be9efa2f4a2d4d0fb027
Devashish Patel <dev.patel8787@gmail.com>
2021-05-07 10:51:29 -0400

    added header

--- packages/Components/3-organisms/Header/Header.jsx
+++ packages/Components/3-organisms/Header/Header.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import styles from "./Header.module.scss"
 import Kalpesh from "./kalpesh.jpeg"
 
+let x;
+
 export default function Header() {
     return (
         <div className={styles.Header}>
             <div className={styles.Content}>
-            This is the Header
+            <div className={styles.Heading}>Kaps Patel Realtor <sup>®</sup></div>
+            <div className={styles.SubHeading}>Please fill out the information below</div>
             </div>
             <div className={styles.Image}>
             <img className={styles.HeadShot}src={Kalpesh}/>
